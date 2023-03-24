e0126781bfcee6d4af60716cac6f3171c77b4289
mate-v <99695898+mate-v@users.noreply.github.com>
2023-03-24 23:45:13 +0100

    Create CreatePhoneNumber.java

---
+++ CreatePhoneNumber.java
@@ -0,0 +1,37 @@
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers
+in the form of a phone number.
+
+
+Example
+Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+
+Don't forget the space after the closing parentheses!
+
+10:22
+ */
+public class Temp {
+    public static void main(String[] args) {
+        System.out.println(createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+    }
+
+    public static String createPhoneNumber(int[] numbers) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("(");
+        for (int i = 0; i < 3; i++) {
+            stringBuilder.append(numbers[i]);
+        }
+        stringBuilder.append(") ");
+        for (int i = 3; i < 6; i++) {
+            stringBuilder.append(numbers[i]);
+        }
+        stringBuilder.append("-");
+        for (int i = 6; i < 10; i++) {
+            stringBuilder.append(numbers[i]);
+        }
+
+        return stringBuilder.toString();
+    }
+}
