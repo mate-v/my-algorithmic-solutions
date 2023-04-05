b16ea3c2e5a5ea10514cf428fe57e25eb1a6f096
mate-v <99695898+mate-v@users.noreply.github.com>
2023-04-05 21:14:50 +0200

    time limit exc. - new variations

--- MinMovesToEqArrEl.java
+++ MinMovesToEqArrEl.java
@@ -3,7 +3,106 @@ import java.util.Arrays;
 https://leetcode.com/problems/minimum-moves-to-equal-array-elements/
 */
 class MinMovesToEqArrEl {
-     public static int minMoves(int[] nums) {
+    public static void main(String[] args) {
+        System.out.println(minMoves1(new int[]{1, 1000000000}));
+        //System.out.println(minMoves1(new int[]{1, 2}));
+        //System.out.println(minMoves1(new int[]{1, 2, 3}));
+        //System.out.println(minMoves2(new int[]{1, 2, 3}));
+    }
+
+    public static int minMoves1(int[] nums) {
+        int counter = 0;
+        Arrays.sort(nums);
+        boolean equivalent = false;
+
+        if (nums[0] == nums[nums.length - 1]) {
+            return counter;
+        }
+
+        int indexOf = nums[nums.length - 1];
+
+        while (!equivalent) {
+            equivalent = true;
+
+            for (int i = nums.length - 1; i > 0; i--) {
+                if (nums[i - 1] != nums[i]) {
+                    equivalent = false;
+                    if (nums[i] > nums[i - 1]) {
+                        indexOf = i;
+                    } else {
+                        indexOf = i - 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                if (i != indexOf) {
+                    nums[i]++;
+                }
+            }
+
+            if (!equivalent) {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    public static int minMoves2(int[] nums) {
+        int counter = 0;
+        Arrays.sort(nums);
+        boolean equivalent = false;
+
+        if (nums[0] == nums[nums.length - 1]) {
+            return counter;
+        }
+
+        int highest = nums[nums.length - 1];
+        int indexOfHighest = nums.length - 1;
+
+        while (!equivalent) {
+            equivalent = true;
+
+            for (int i = nums.length - 1; i > 0; i--) {
+                if (nums[i] != nums[i - 1]) {
+                    equivalent = false;
+                    break;
+                }
+            }
+
+            if (!equivalent) {
+                for (int i = 0; i < nums.length; i++) {
+                    if (nums[i] > highest) {
+                        highest = nums[i];
+                        indexOfHighest = i;
+                        break;
+                    }
+                }
+
+                if (indexOfHighest != 0) {
+                    for (int i = 0; i < indexOfHighest; i++) {
+                        nums[i]++;
+                    }
+                    if (indexOfHighest != nums.length - 1) {
+                        for (int i = indexOfHighest + 1; i < nums.length; i++) {
+                            nums[i]++;
+                        }
+                    }
+                } else {
+                    for (int i = 1; i < nums.length; i++) {
+                        nums[i]++;
+                    }
+                }
+                counter++;
+            }
+
+        }
+
+        return counter;
+    }
+
+    public static int minMoves3(int[] nums) {
         int counter = 0;
 
         HashMap<Integer, Integer> hMap = new HashMap<Integer, Integer>();
